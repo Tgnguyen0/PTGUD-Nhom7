6a5d984f124629cf614d8acf11c0e07ef8600ba4
NguyenVanTong <22662921.tong@student.iuh.edu.vn>
2025-03-21 00:54:00 +0700

    cập nhật trang movies và trang series

--- src/Componets/HeaderItem.jsx
+++ src/Componets/HeaderItem.jsx
@@ -13,16 +13,16 @@ function HeaderItem({name,Icon}) {
         navigate('/search');
         break;
       case 'WATCH LIST':
-        alert("None done yet");
+        navigate('/watchlist');
         break;
       case 'ORIGINALS':
-        alert("None done yet");
+        navigate('/originals');
         break;
       case 'MOVIES':
-        alert("None done yet");
+        navigate('/movies');
         break;
       case 'SERIES':
-        alert("None done yet");
+        navigate('/series');
         break;
       case 'ABOUT':
         navigate('/about');
@@ -33,12 +33,13 @@ function HeaderItem({name,Icon}) {
   };
 
   return (
-    <div className='text-white flex items-center gap-3
-    text-[15px] font-semibold cursor-pointer hover:underline
-    underline-offset-8 mb-2'
-    onClick={handleClick}
+    <div 
+      className='text-white flex items-center gap-3
+      text-[15px] font-semibold cursor-pointer hover:underline
+      underline-offset-8 mb-2 transition-all duration-300'
+      onClick={handleClick}
     >
-        <Icon/>
+        <Icon className="h-5 w-5"/>
         <h2 className=''>{name}</h2>
     </div>
   )
